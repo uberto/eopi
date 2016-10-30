9eebde568940b25c92170dacdf0a5e6d4c83a907
Uberto Barbini <uberto@ubiland.net>
2016-10-30 22:14:16 +0000

    sequence incremental

--- src/main/java/com/gamasoft/eopi/IncrementingSequence.java
+++ src/main/java/com/gamasoft/eopi/IncrementingSequence.java
@@ -8,7 +8,60 @@ import java.util.List;
 public class IncrementingSequence {
 
     public static List<Integer> findLongestIncrementingSequence(List<Integer> seq) {
-        return seq;
+
+        int from = 0;
+        int last = Integer.MIN_VALUE;
+        int maxStreak = 0;
+        int currStreak = 0;
+
+        for(int i = 0; i < seq.size(); i++){
+
+            int curr = seq.get(i);
+
+            if (curr < last ) {
+                currStreak = 1;
+            } else {
+                currStreak++;
+                if (currStreak > maxStreak){
+                    maxStreak = currStreak;
+                    from = i - currStreak + 1;
+                }
+            }
+
+            last = curr;
+        }
+
+        return seq.subList(from, from + maxStreak);
     }
+
+
+    public static List<Integer> findLongestNonContIncrementingSequence(List<Integer> seq) {
+
+        int from = 0;
+        int last = Integer.MIN_VALUE;
+        int maxStreak = 0;
+        int currStreak = 0;
+
+        for(int i = 0; i < seq.size(); i++){
+
+            int curr = seq.get(i);
+
+//            if (curr < last ) {
+//                currStreak = 1;
+//            } else {
+//                currStreak++;
+//                if (currStreak > maxStreak){
+//                    maxStreak = currStreak;
+//                    from = i - currStreak + 1;
+//                }
+//            }
+//
+            last = curr;
+        }
+
+        return seq.subList(from, from + maxStreak);
+    }
+
+
 
 }
--- src/test/java/com/gamasoft/eopi/IncrementingSequenceTest.java
+++ src/test/java/com/gamasoft/eopi/IncrementingSequenceTest.java
@@ -24,4 +24,29 @@ public class IncrementingSequenceTest {
         Assert.assertEquals(3, r.size());
 
     }
+
+    @Test
+    public void simpleSeq2() throws Exception {
+
+        List<Integer> seq = new ArrayList(Arrays.asList(1, 2, 3, 2, 3, 5, 10, 9));
+
+        List<Integer> r = IncrementingSequence.findLongestIncrementingSequence(seq);
+
+        Assert.assertEquals(4, r.size());
+        Assert.assertEquals(10, r.get(3).intValue());
+
+    }
+
+
+    @Test
+    public void simpleSeq3() throws Exception {
+
+        List<Integer> seq = new ArrayList(Arrays.asList(10, 9, 8, 7, 6));
+
+        List<Integer> r = IncrementingSequence.findLongestIncrementingSequence(seq);
+
+        Assert.assertEquals(1, r.size());
+
+    }
+
 }
