618b857dc681e4691bf3a34113b104cedde53339
Uberto Barbini <uberto@ubiland.net>
2016-11-01 21:07:34 +0000

    tree seach

---
+++ src/main/java/com/gamasoft/eopi/Tree.java
@@ -0,0 +1,52 @@
+package com.gamasoft.eopi;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * Created by uberto on 01/11/16.
+ */
+public class Tree<T extends Comparable> {
+
+    public static class Node<T extends Comparable> implements Comparable<Node<T>> {
+
+        Set<Node<T>> children = new TreeSet<>();
+        T value;
+        public Node(T value){
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node<T> o) {
+            return this.value.compareTo(o.value);
+        }
+    }
+    Node<T> root;
+
+    public void visitDeepFirst(Consumer<T> action){
+        visitDF(root, action);
+    }
+
+    private void visitDF(Node<T> node, Consumer<T> action) {
+        action.accept(node.value);
+        for(Node<T> c: node.children)
+            visitDF(c, action);
+
+    }
+
+    public void visitBreadthFirst(Consumer<T> action) {
+        Deque<Node<T>> queue = new ArrayDeque<>();
+        queue.addLast(root);
+
+        while(!queue.isEmpty()){
+            Node<T> node = queue.removeFirst();
+            action.accept(node.value);
+            for(Node<T> c: node.children)
+                queue.addLast(c);
+        }
+
+
+    }
+
+
+}
---
+++ src/test/java/com/gamasoft/eopi/TreeTest.java
@@ -0,0 +1,58 @@
+package com.gamasoft.eopi;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uberto on 01/11/16.
+ */
+public class TreeTest {
+
+    @Test
+    public void visitDF() {
+        Tree<Integer> t = createSimpleTree();
+
+        StringBuilder visited = new StringBuilder();
+
+        t.visitDeepFirst(visited::append);
+
+        Assert.assertEquals("013425", visited.toString());
+
+    }
+
+
+    @Test
+    public void visitBF() {
+        Tree<Integer> t = createSimpleTree();
+
+        StringBuilder visited = new StringBuilder();
+
+        t.visitBreadthFirst(visited::append);
+
+        Assert.assertEquals("012345", visited.toString());
+
+    }
+
+
+    private Tree<Integer> createSimpleTree() {
+    /*
+        0
+        1     2
+        3 4   5
+    */
+
+        Tree.Node<Integer> n1 = new Tree.Node<>(1);
+        n1.children.add(new Tree.Node<>(3));
+        n1.children.add(new Tree.Node<>(4));
+        Tree.Node<Integer> n2 = new Tree.Node<>(2);
+        n2.children.add(new Tree.Node<>(5));
+
+        Tree<Integer> t = new Tree<>();
+        t.root = new Tree.Node<>(0);
+        t.root.children.add(n1);
+        t.root.children.add(n2);
+        return t;
+    }
+}
