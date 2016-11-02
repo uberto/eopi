6f38bb716b258879b777a8053f18399cdc4cd588
Uberto Barbini <uberto@ubiland.net>
2016-11-02 21:34:51 +0000

    circular arrays

---
+++ src/main/java/com/gamasoft/eopi/CircularQueue.java
@@ -0,0 +1,34 @@
+package com.gamasoft.eopi;
+
+/**
+ * Created by uberto on 02/11/16.
+ */
+public class CircularQueue<T> {
+
+    private final T[] buffer;
+    private int head;
+    private int tail;
+
+    public CircularQueue(int size){
+        buffer = (T[]) new Object[size];
+        head = 0;
+        tail = 0;
+    }
+
+    public void enqueue(T i) {
+        buffer[head] = i;
+        head++;
+        head %= buffer.length;
+        if (head == tail)
+            throw new RuntimeException("buffer size limit!");
+    }
+
+    public T deque() {
+        T r = buffer[tail];
+        tail++;
+        tail %= buffer.length;
+        if (head == tail)
+            throw new RuntimeException("buffer size limit!");
+        return r;
+    }
+}
---
+++ src/test/java/com/gamasoft/eopi/CircularQueueTest.java
@@ -0,0 +1,49 @@
+package com.gamasoft.eopi;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uberto on 02/11/16.
+ */
+public class CircularQueueTest {
+
+
+    @Test
+    public void writeAndRead() throws Exception {
+
+        CircularQueue<String> circ = new CircularQueue<String>(10);
+
+        circ.enqueue("-3");
+        circ.enqueue("-2");
+        circ.enqueue("-1");
+        for(int i = 0; i < 1000; i++){
+            circ.enqueue(String.valueOf(i));
+            String r = circ.deque();
+
+            Assert.assertEquals(String.valueOf(i-3), r);
+        }
+
+    }
+
+
+    @Test
+    public void tooManyWrites() throws Exception {
+
+        CircularQueue<String> circ = new CircularQueue<String>(10);
+
+        for(int i = 0; i < 9; i++){
+            circ.enqueue(String.valueOf(i));
+        }
+
+        try {
+            circ.enqueue(String.valueOf(10));
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals("buffer size limit!", e.getMessage());
+        }
+
+    }
+}
