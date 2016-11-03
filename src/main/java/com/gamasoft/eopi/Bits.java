729f854552c06e156b4843df1e130360c9317867
Uberto Barbini <uberto@ubiland.net>
2016-11-03 22:44:25 +0000

    power and parity

--- src/main/java/com/gamasoft/eopi/Bits.java
+++ src/main/java/com/gamasoft/eopi/Bits.java
@@ -1,5 +1,9 @@
 package com.gamasoft.eopi;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by uberto on 31/10/16.
  */
@@ -16,4 +20,46 @@ public class Bits {
         }
         return tot;
     }
+
+    public static short parity(long x) {
+        short p = 0;
+
+        while(x != 0){
+
+            p ^= (x & 1);
+            x >>>= 1;
+        }
+        return p;
+    }
+
+    public static double power(double x, int y) {
+
+//brute force
+//        double r = x;
+//        for (int i = 1; i < y; i++) {
+//            r *= x;
+//        }
+
+        List<Boolean> binary = reverseBinary(y);
+
+        double r = 1;
+        for(Boolean one: binary){
+            r *= r;
+            if (one)
+                r *= x;
+        }
+        return r;
+    }
+
+    private static List<Boolean> reverseBinary(int y) {
+        List<Boolean> res = new ArrayList<>();
+        int r = y;
+        while(r != 0){
+            res.add ((r & 1) == 1);
+            r >>>= 1;
+        }
+        Collections.reverse(res);
+        return res;
+    }
 }
+
--- src/test/java/com/gamasoft/eopi/BitsTest.java
+++ src/test/java/com/gamasoft/eopi/BitsTest.java
@@ -24,8 +24,35 @@ public class BitsTest {
 
         int x = 0b1010101010;
         Assert.assertEquals(5, Bits.countOnes(x));
+    }
 
 
+    //5.1
+    @Test()
+    public void parityCheck(){
+        Assert.assertEquals(0, Bits.parity(0));
+        Assert.assertEquals(0, Bits.parity(3));
+        Assert.assertEquals(0, Bits.parity(257));
+        Assert.assertEquals(1, Bits.parity(1));
+        Assert.assertEquals(1, Bits.parity(2));
+        Assert.assertEquals(1, Bits.parity(2048));
+        Assert.assertEquals(1, Bits.parity(2051));
+    }
+
+
+    //5.7
+    @Test
+    public void power(){
+        Assert.assertEquals(1.0, Bits.power(1, 5));
+        Assert.assertEquals(1.0, Bits.power(7, 0));
+        Assert.assertEquals(7.0, Bits.power(7, 1));
+        Assert.assertEquals(49.0, Bits.power(7, 2));
+        Assert.assertEquals(625.0, Bits.power(5, 4));
+        Assert.assertEquals(4096.0, Bits.power(2, 12));
+        Assert.assertEquals(0.25, Bits.power(0.5, 2));
+        Assert.assertEquals(2.86788021686345E10, Bits.power(123.456, 5));
+        Assert.assertEquals(4294967296.0, Bits.power(2, 32));
+        Assert.assertEquals(1.8446744073709552E19, Bits.power(2, 64));
 
     }
 }
