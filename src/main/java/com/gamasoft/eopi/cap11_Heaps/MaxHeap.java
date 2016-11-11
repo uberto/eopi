e92c920547843201a4cb1e411490f7ed2d0c1897
Uberto Barbini <uberto.gama@gmail.com>
2016-11-11 22:27:20 +0000

    heaps

--- src/main/java/com/gamasoft/eopi/cap11_Heaps/MaxHeap.java
+++ src/main/java/com/gamasoft/eopi/cap11_Heaps/MaxHeap.java
@@ -4,47 +4,139 @@ import com.gamasoft.eopi.cap10_BTrees.BTree;
 
 /**
  * Created by uberto on 10/11/16.
+ *
+ *
+ *
+ *
  */
 public class MaxHeap<T extends Comparable> {
 
-    private BTree<T> head = new BTree<>(null, null, null);;
+    private BTree<T> head = null;
+
+    /*
+    push 1..5
+
+    1
+
+    2
+    1
+
+    3
+    2 1
+
+    4
+    3  2
+    1
+
+    5
+    4  3
+    2  1
+     */
 
     public void addAll(T[] array) {
 
-
         for (T e: array){
-            add(head, e);
+            BTree<T> newNode = new BTree<>(e, null, null);
+            if (head == null)
+                head = new BTree<>(e, null, null);
+            else if (bigger(newNode, head)){
+                newNode.left = head;
+                newNode.right = head.left;
+                head.left = head.right;
+                head.right = null;
+                head = newNode;
+            } else
+                add(head, newNode);
         }
     }
 
-    private void add(BTree<T> n, T e) {
+    private void add(BTree<T> n, BTree<T> newNode) {
 
-        BTree<T> newNode = new BTree<>(e, null, null);
         if(n.left == null){
             n.left = newNode;
         } else if(n.right == null){
             n.right = newNode;
         } else {
-            if (n.left.value.compareTo(n.right.value) > 0 && (e.compareTo(n.left.value) > 0)) {
-                newNode.right = n.left;
+            if (bigger(newNode, n.left)) {
+                newNode.left = n.left;
+                newNode.right = n.left.left;
+                n.left.left = null;
                 n.left = newNode;
-            } else if (n.right.value.compareTo(n.left.value) > 0 && (e.compareTo(n.right.value) > 0)) {
+            } else if (bigger(newNode, n.right)) {
                 newNode.left = n.right;
+                newNode.right = n.right.left;
+                n.right.left = null;
                 n.right = newNode;
-            } else if ((e.compareTo(n.right.value) < 0)) {
-                add(n.right, e);
+            } else if (bigger(n.right, n.left)) {
+                add(n.right, newNode);
             } else {
-                add(n.left, e);
+                add(n.left, newNode);
             }
         }
     }
 
+    private boolean bigger(BTree<T> left, BTree<T> right) {
+        if (left == null)
+            return false;
+        if (right == null)
+            return true;
+
+        return left.value.compareTo(right.value) > 0;
+    }
+
     public T pop() {
-//        BTree<T> node = this.head;
-//        if (node.left.value.compareTo(node.right.value) > 0){
-//            node.
-//        }
-//
-      return head.right.value;
+        BTree<T> res = head;
+        head = popFrom(head);
+        return res.value;
+    }
+
+    private BTree<T> popFrom(BTree<T> node) {
+        if (node == null)
+            return null;
+        BTree<T> res;
+        if (bigger(node.left, node.right)) {
+            res = node.left;
+            if (res != null) {
+                res.right = popFrom(node.left);
+                res.left = node.right;
+            }
+
+        }else {
+            res = node.right;
+            if (res != null){
+                res.right = popFrom(node.right);
+                res.left = node.left;
+            }
+        }
+
+        return res;
     }
 }
+
+
+/*
+
+15
+7     10
+4 3   9 8
+
+pop
+
+10
+7    9
+4 3  8
+
+push 5
+10
+7    9
+5 3  8
+4
+
+push 16
+16
+7    10
+5 3  9 8
+4
+
+
+ */
--- src/test/java/com/gamasoft/eopi/cap11_Heaps/SortArrayTest.java
+++ src/test/java/com/gamasoft/eopi/cap11_Heaps/SortArrayTest.java
@@ -13,19 +13,60 @@ public class SortArrayTest {
     //11.1 (modified)
 
     @Test
-    public void sort() throws Exception {
+    public void sortAFew() throws Exception {
 
-        Integer[] a = new Integer[]{1, 16, 19, 10};
+        Integer[] a = new Integer[]{1, 2, 3, 4, 5};
 
         MaxHeap<Integer> heap = new MaxHeap<>();
 
         heap.addAll(a);
 
-        Assert.assertEquals(19, heap.pop().intValue());
-//        Assert.assertEquals(16, heap.pop().intValue());
-//        Assert.assertEquals(10, heap.pop().intValue());
-//        Assert.assertEquals(1, heap.pop().intValue());
+        Assert.assertEquals(5, heap.pop().intValue());
+        Assert.assertEquals(4, heap.pop().intValue());
+        Assert.assertEquals(3, heap.pop().intValue());
+        Assert.assertEquals(2, heap.pop().intValue());
+        Assert.assertEquals(1, heap.pop().intValue());
 
+    }
+
+
+    @Test
+    public void sortAFewOrdered() throws Exception {
+
+        Integer[] a = new Integer[]{5, 4, 3, 2, 1};
+
+        MaxHeap<Integer> heap = new MaxHeap<>();
+
+        heap.addAll(a);
+
+        Assert.assertEquals(5, heap.pop().intValue());
+        Assert.assertEquals(4, heap.pop().intValue());
+        Assert.assertEquals(3, heap.pop().intValue());
+        Assert.assertEquals(2, heap.pop().intValue());
+        Assert.assertEquals(1, heap.pop().intValue());
+
+    }
+
+
+    @Test
+    public void sortMany() throws Exception {
+
+        Integer[] a = new Integer[]{1, 8, 6, 10, 5, 7, 4, 2, 3, 9};
+
+        MaxHeap<Integer> heap = new MaxHeap<>();
+
+        heap.addAll(a);
+
+        Assert.assertEquals(10, heap.pop().intValue());
+        Assert.assertEquals(9, heap.pop().intValue());
+        Assert.assertEquals(8, heap.pop().intValue());
+        Assert.assertEquals(7, heap.pop().intValue());
+        Assert.assertEquals(6, heap.pop().intValue());
+        Assert.assertEquals(5, heap.pop().intValue());
+        Assert.assertEquals(4, heap.pop().intValue());
+        Assert.assertEquals(3, heap.pop().intValue());
+        Assert.assertEquals(2, heap.pop().intValue());
+        Assert.assertEquals(1, heap.pop().intValue());
 
     }
 }
