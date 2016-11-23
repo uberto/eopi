e231aa682911b8911a329ce668f8ed6e29870975
Uberto Barbini <uberto.gama@gmail.com>
2016-11-23 23:23:32 +0000

    football score

--- src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/FootballScoreTest.java
+++ src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/FootballScoreTest.java
@@ -26,18 +26,22 @@ public class FootballScoreTest {
 
     }
     @Test
-    public void twoScore() throws Exception {
-     Assert.assertEquals(2, FootballScore.scoreCombinations(6));
-
+    public void twoOrThreeScore() throws Exception {
+        Assert.assertEquals(2, FootballScore.scoreCombinations(6));
+        Assert.assertEquals(2, FootballScore.scoreCombinations(7));
+        Assert.assertEquals(2, FootballScore.scoreCombinations(8));
+        Assert.assertEquals(3, FootballScore.scoreCombinations(9));
+        Assert.assertEquals(3, FootballScore.scoreCombinations(10));
     }
 
     @Test
     public void biggerScore() throws Exception {
 
-        Assert.assertEquals(2, FootballScore.scoreCombinations(7));
-        Assert.assertEquals(2, FootballScore.scoreCombinations(8));
-        Assert.assertEquals(3, FootballScore.scoreCombinations(9));
-        Assert.assertEquals(3, FootballScore.scoreCombinations(10));
+
+        Assert.assertEquals(129, FootballScore.scoreCombinations(98));
+        Assert.assertEquals(131, FootballScore.scoreCombinations(99));
+        Assert.assertEquals(134, FootballScore.scoreCombinations(100));
+        Assert.assertEquals(136, FootballScore.scoreCombinations(101));
 
     }
 }
