c865ae1ca3197582dc50a63c0f0cc50f5c425d32
Uberto Barbini <uberto.gama@gmail.com>
2016-11-24 22:34:45 +0000

    clean up

--- src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/LevenshteinDistanceTest.java
+++ src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/LevenshteinDistanceTest.java
@@ -66,6 +66,6 @@ public class LevenshteinDistanceTest {
 
         Assert.assertEquals(11, LevenshteinDistance.measure(
                 "In information theory and computer science, the Levenshtein distance is a string metric for measuring the difference between two sequences. Informally, the Levenshtein distance between two words is the minimum number of single-character edits (i.e. insertions, deletions or substitutions) required to change one word into the other.",
-                "In information teory and compiuter science, the Levenshtein distance is a string metric for mesuring the difference between two sequences. Informally, the Levnenshtein distance between two words is the minimum number of single-character edits (i.e. insertions, deleshions or sostitutions) required to change 1 word into the other."));
+                "In information teory and compiuter science, the Levenshtein distance is a string metric for mesuring the difference between two sequences. Informally, the Levnenshtein distance between two words is the minimum number of single-character edits (i.e. insertions, deleshions or sostitutions) required to change 1 word into the others.."));
     }
 }
