2a4995d5da262444510298c166b9b2fa4ba1ae21
Uberto Barbini <uberto.gama@gmail.com>
2016-11-27 22:14:12 +0000

    knapsack

--- src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/IncrementingSequenceTest.java
+++ src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/IncrementingSequenceTest.java
@@ -10,6 +10,8 @@ import java.util.List;
 /**
  * Created by uberto on 30/10/16.
  */
+
+//17.12
 public class IncrementingSequenceTest {
 
     @Test
--- src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/KnapsackTest.java
+++ src/test/java/com/gamasoft/eopi/cap17_DynamicProgramming/KnapsackTest.java
@@ -10,6 +10,7 @@ import java.util.Set;
 /**
  * Created by uberto on 27/11/16.
  */
+//17.6
 public class KnapsackTest {
 
     @Test
