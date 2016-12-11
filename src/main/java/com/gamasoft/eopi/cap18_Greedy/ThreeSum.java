2931031646486d85d662ba89b493bbb1c1b1d6ce
Uberto Barbini <uberto.gama@gmail.com>
2016-12-11 22:36:20 +0000

    three sum fix

--- src/main/java/com/gamasoft/eopi/cap18_Greedy/ThreeSum.java
+++ src/main/java/com/gamasoft/eopi/cap18_Greedy/ThreeSum.java
@@ -9,29 +9,46 @@ import java.util.Set;
  * Created by uberto on 11/12/16.
  */
 public class ThreeSum {
-    public static boolean exists(Set<Integer> set, int sum) {
+    public static boolean exists3Sum(Set<Integer> set, int sum) {
 
+        if (sum % 3 == 0 && set.contains(sum / 3)) {
+            return true;
+        }
         List<Integer> l = new ArrayList<>(set);
         Collections.sort(l);
-
-        if (l.get(0) * 3 > sum)
+        if (l.get(0) * 3 > sum) {
             return false;
-        if (l.get(l.size() - 1) * 3 < sum)
+        }
+        if (l.get(l.size() - 1) * 3 < sum) {
             return false;
-
-        if (sum % 3 == 0 && l.contains(sum / 3))
-            return true;
+        }
 
         for (int i = 0; i<l.size(); i++){
-            int x = sum - l.get(i)*2;
-            if (l.contains(x))
+            int x = sum - l.get(i);
+            if (exists2Sum(l, x))
                 return true;
-            if (x <= 0)
+            if (x <= l.get(0))
                 break;
         }
 
-
-
-            return false;
+        return false;
     }
+
+    private static boolean exists2Sum(List<Integer> orderedList, int sum) {
+        int right = orderedList.size() - 1;
+        int left = 0;
+
+
+        while (left < right){
+            int sumE = orderedList.get(left) + orderedList.get(right);
+            if (sumE < sum)
+                left++;
+            else if (sumE > sum)
+                right--;
+            else
+                return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/gamasoft/eopi/cap18_Greedy/ThreeSumTest.java
+++ src/test/java/com/gamasoft/eopi/cap18_Greedy/ThreeSumTest.java
@@ -16,15 +16,15 @@ public class ThreeSumTest {
     public void minimumSet() throws Exception {
         Set<Integer> s = new HashSet<>(Arrays.asList(1,2,3));
 
-        Assert.assertFalse(ThreeSum.exists(s, 10));
-        Assert.assertTrue(ThreeSum.exists(s, 9));
-        Assert.assertTrue(ThreeSum.exists(s, 8));
-        Assert.assertTrue(ThreeSum.exists(s, 7));
-        Assert.assertTrue(ThreeSum.exists(s, 6));
-        Assert.assertTrue(ThreeSum.exists(s, 5));
-        Assert.assertTrue(ThreeSum.exists(s, 4));
-        Assert.assertTrue(ThreeSum.exists(s, 3));
-        Assert.assertFalse(ThreeSum.exists(s, 2));
+        Assert.assertFalse(ThreeSum.exists3Sum(s, 10));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 9));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 8));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 7));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 6));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 5));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 4));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 3));
+        Assert.assertFalse(ThreeSum.exists3Sum(s, 2));
 
     }
 
@@ -34,13 +34,13 @@ public class ThreeSumTest {
     public void biggerSet() throws Exception {
         Set<Integer> s = new HashSet<>(Arrays.asList(2,3,8,21,55));
 
-        Assert.assertFalse(ThreeSum.exists(s, 100));
-        Assert.assertTrue(ThreeSum.exists(s, 97));
-        Assert.assertTrue(ThreeSum.exists(s, 13));
-        Assert.assertTrue(ThreeSum.exists(s, 79));
-        Assert.assertTrue(ThreeSum.exists(s, 26));
-        Assert.assertTrue(ThreeSum.exists(s, 65));
-        Assert.assertFalse(ThreeSum.exists(s, 5));
+        Assert.assertFalse(ThreeSum.exists3Sum(s, 100));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 97));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 13));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 79));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 26));
+        Assert.assertTrue(ThreeSum.exists3Sum(s, 65));
+        Assert.assertFalse(ThreeSum.exists3Sum(s, 5));
 
     }
 }
